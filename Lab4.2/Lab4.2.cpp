119d63ddc40af1f5bbe63f4117092daa3aee5872
Chiffilin <dimaaitbaev42@gmail.com>
2020-04-23 19:50:36 +0300

    Добавьте файлы проекта.

---
+++ Lab4.2/Lab4.2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string a;
+    cout << "Введите строку: ";
+    getline(cin, a);
+    string b;
+    b += a[0];
+    for (int i = 1; i < a.length(); i++)
+    {
+        if ((a[i] != ' ') || ((a[i] == ' ') and (a[i - 1] != ' '))) b += a[i];
+    }
+    cout << "  " << endl;
+        cout << b;
+
+
+    return 0;
+}
